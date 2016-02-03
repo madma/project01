f3e71f29e982c348939fdb1614444985ebc16429
Michael Duran <mad2116@gmail.com>
2016-02-03 11:32:23 -0800

    rethink piece model and rewrite constructor as test

--- js/test.js
+++ js/test.js
@@ -32,49 +32,161 @@ var Opiece = function() {
   this.coords = [[0, 3], [0, 4], [1, 3], [1, 4]];
 }
 
-Opiece.prototype.getCoords = function() {return this.coords;}
-
 var Ipiece = function() {
   this.coords = [[0, 3], [0, 4], [0, 5], [0, 6]];
   this.pivotPt = this.coords[2];
 }
-
-Ipiece.prototype.getCoords = function() {return this.coords;}
 
 var Spiece = function() {
   this.coords = [[0, 3], [0, 4], [1, 2], [1, 3]];
   this.pivotPt = this.coords[0];
 }
 
-Spiece.prototype.getCoords = function() {return this.coords;}
-
 var Zpiece = function() {
   this.coords = [[0, 2], [0, 3], [1, 3], [1, 4]];
   this.pivotPt = this.coords[1];
 }
-
-Zpiece.prototype.getCoords = function() {return this.coords;}
 
 var Lpiece = function() {
   this.coords = [[0, 2], [0, 3], [0, 4], [1, 2]];
   this.pivotPt = this.coords[1];
 }
 
-Lpiece.prototype.getCoords = function() {return this.coords;}
-
 var Jpiece = function() {
   this.coords = [[0, 2], [0, 3], [0, 4], [1, 4]];
   this.pivotPt = this.coords[1];
 }
-
-Jpiece.prototype.getCoords = function() {return this.coords;}
 
 var Tpiece = function() {
   this.coords = [[0, 2], [0, 3], [0, 4], [1, 3]];
   this.pivotPt = this.coords[1];
 }
 
-Tpiece.prototype.getCoords = function() {return this.coords;}
+
+//////////////
+
+var compare = function(a1, a2) {
+  return a1.toString() === a2.toString();
+};
+
+var isIn = function(coord, listOfCoords) {
+  for (var i = 0; i < listOfCoords.length; i++) {
+    if (compare(coord, listOfCoords[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+function Piece(type) {
+  this.type = type.toUpperCase();
+  switch (this.type) {
+    case "O":
+
+      // ORIENTATION 0
+      // 0 A
+      // 1   X X
+      // 2   X X
+      // 3
+      //   0 1 2 3
+
+      this.coords  = [
+        [ [1, 1], [2, 1], [1, 2], [2, 2] ]
+      ];
+      this.orientation = 0;
+      this.anchor = [4, -1];
+      break;
+    case "I":
+
+      // ORIENTATION 0
+      // 0 A
+      // 1 X X X X
+      // 2
+      // 3
+      //   0 1 2 3
+
+      // ORIENTATION 1
+      // 0 A   X
+      // 1     X
+      // 2     X
+      // 3     X
+      //   0 1 2 3
+
+      this.coords  = [
+        [ [0, 1], [1, 1], [2, 1], [3, 1] ],
+        [ [2, 0], [2, 1], [2, 2], [2, 3] ]
+      ];
+      this.orientation = 0;
+      this.anchor = [4, -1];
+
+      break;
+    case "S":
+      this.coords  = [[0, 3], [0, 4], [1, 2], [1, 3]];
+      this.pivotPt = this.coords[0];
+      break;
+    case "Z":
+      this.coords  = [[0, 2], [0, 3], [1, 3], [1, 4]];
+      this.pivotPt = this.coords[1];
+      break;
+    case "L":
+      this.coords  = [[0, 2], [0, 3], [0, 4], [1, 2]];
+      this.pivotPt = this.coords[1];
+      break;
+    case "J":
+      this.coords  = [[0, 2], [0, 3], [0, 4], [1, 4]];
+      this.pivotPt = this.coords[1];
+      break;
+    case "T":
+      this.coords  = [[0, 2], [0, 3], [0, 4], [1, 3]];
+      this.pivotPt = this.coords[1];
+      break;
+  }
+}
+
+Piece.prototype.getInternalCoords = function() {
+  return this.coords.slice();
+};
+
+Piece.prototype.getBoardCoords = function() {
+  var currentCoords = this.coords[this.orientation];
+
+  return [
+    [currentCoords[0][0] + this.anchor[0], currentCoords[0][1] + this.anchor[1]],
+    [currentCoords[1][0] + this.anchor[0], currentCoords[1][1] + this.anchor[1]],
+    [currentCoords[2][0] + this.anchor[0], currentCoords[2][1] + this.anchor[1]],
+    [currentCoords[3][0] + this.anchor[0], currentCoords[3][1] + this.anchor[1]]
+  ];
+};
+
+Piece.prototype.calculateCells = function() {
+  var value = this.type;
+  var v = value;
+  var currentOrientation    = this.orientation;
+  var currentInternalCoords = this.coords[currentOrientation];
+  var c = currentInternalCoords;
+
+  console.log(` ORIENT: ${currentOrientation} `);
+  console.log(` ${this.anchor[1] + 0}   ${isIn([1,0], c) ? v : " "} ${isIn([2,0], c) ? v : " "} ${isIn([3,0], c) ? v : " "} `);
+  console.log(` ${this.anchor[1] + 1} ${isIn([0,1], c) ? v : " "} ${isIn([1,1], c) ? v : " "} ${isIn([2,1], c) ? v : " "} ${isIn([3,1], c) ? v : " "} `);
+  console.log(` ${this.anchor[1] + 2}   ${isIn([1,2], c) ? v : " "} ${isIn([2,2], c) ? v : " "} ${isIn([3,2], c) ? v : " "} `);
+  console.log(` ${this.anchor[1] + 3}     ${isIn([2,3], c) ? v : " "}   `);
+  console.log(`   ${this.anchor[0] + 0} ${this.anchor[0] + 1} ${this.anchor[0] + 2} ${this.anchor[0] + 3} `);
+};
+
+Piece.prototype.rotate = function() {
+  if (this.orientation < this.coords.length - 1) {
+    this.orientation++;
+  } else {
+    this.orientation = 0;
+  }
+};
+
+Piece.random = function() {
+  var types = ["O", "I", "S", "Z", "L", "J", "T"];
+  var index =  Math.floor(Math.random()*types.length);
+  return new Piece(types[index]);
+};
 
 var pieces = [Opiece, Ipiece, Spiece, Zpiece, Lpiece, Jpiece, Tpiece];
 
